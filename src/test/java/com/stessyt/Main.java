74879571555f36bfd003c94cad6e99a0ca0bef17
StessyT <wildorhid@gmail.com>
2021-08-15 18:48:35 +0300

    Added Vessel class and two new vessels

--- src/test/java/com/stessyt/Main.java
+++ src/test/java/com/stessyt/Main.java
@@ -2,14 +2,12 @@ package com.stessyt;
 
 public class Main {
     public static void main(String[] args) {
-        Vessel vessel = new Vessel();
-        vessel.vesselType = "Survey vessel";
-        vessel.yearBuild = 1982;
-        vessel.portOfRegistry = "Tuvalu";
-        vessel.maxSpeed = 10;
-        vessel.lengthOverall = 40;
-        vessel.usedForSailing = false;
-
-        System.out.println(vessel.vesselType);
+        Vessel vessel1 = new Vessel("Survey vessel", 1982, "Tuvalu", 10, 40, false);
+        Vessel vessel2 = new Vessel("Hanse 460", 2018, "Tallinn", 7, 45, true);
+        System.out.println(vessel1.vesselType);
+        System.out.println(vessel1.getVesselType());
+        vessel1.setVesselType("Sailing");
+        System.out.println(vessel1.getVesselType());
+        vessel2.sayVesselProperties();
     }
 }
--- src/test/java/com/stessyt/Vessel.java
+++ src/test/java/com/stessyt/Vessel.java
@@ -8,12 +8,25 @@ public class Vessel {
     int lengthOverall;
     boolean usedForSailing;
 
-//    public Vessel (String vesselType, int yearBuild, String portOfRegistry, String maxSpeed, int lengthOverall, boolean usedForSailing) {
-//        this.vesselType = vesselType;
-//        this.yearBuild = yearBuild;
-//        this.portOfRegistry = portOfRegistry;
-//        this. maxSpeed = maxSpeed;
-//        this.lengthOverall = lengthOverall;
-//        this.usedForSailing = usedForSailing;
+    public Vessel (String vesselType, int yearBuild, String portOfRegistry, int maxSpeed, int lengthOverall, boolean usedForSailing) {
+        this.vesselType = vesselType;
+        this.yearBuild = yearBuild;
+        this.portOfRegistry = portOfRegistry;
+        this.maxSpeed = maxSpeed;
+        this.lengthOverall = lengthOverall;
+        this.usedForSailing = usedForSailing;
     }
+
+    String getVesselType() {
+        return vesselType;
+    }
+
+    void setVesselType(String newVesselType) {
+        this.vesselType = newVesselType;
+    }
+
+    void sayVesselProperties() {
+        System.out.println("vessel type" + " " + "'" + vesselType + "'" + " -" + " year " + yearBuild);
+    }
+}
 
